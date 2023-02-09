302dc8db317ae453242edad7f3ac95b0784e15de
Christo Grabowski <christo.grab@gmail.com>
2023-02-09 12:44:03 -0600

    added background image for earth and resized all planets

--- src/components/Viewport/index.js
+++ src/components/Viewport/index.js
@@ -5,7 +5,7 @@ const Viewport = () => {
 
   return (
     <div className="main-page-container">
-      <div className="title">Solarium v0.3</div>
+      <div className="title">Solarium v0.4</div>
       <div className="creator">Made by Christo Grabowski</div>
       <Sun />
     </div>

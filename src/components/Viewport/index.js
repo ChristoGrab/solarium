4691dd2b46cf79cfb102a22ab0b02bfa7744b3ce
Christo Grabowski <christo.grab@gmail.com>
2023-02-15 09:58:32 -0600

    resizing elements to fit various screen sizes

--- src/components/Viewport/index.js
+++ src/components/Viewport/index.js
@@ -5,11 +5,11 @@ const Viewport = () => {
 
   return (
     <div className="main-page-container">
-      <div className="title">Solarium v0.8</div>
+      <div className="title">Solarium v0.8
       <div className="creator">Made by
         <a className="github-link" href="https://christograb.github.io" target="_blank" rel="noreferrer">Christo Grabowski</a>
       </div>
-      {/* <Sun /> */}
+      </div>
     </div>
   )
 }

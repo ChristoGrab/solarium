dbf28c8ba6dd312ad1e5aae80c1b02e6c88f7b00
Christo Grabowski <christo.grab@gmail.com>
2023-02-28 18:43:50 -0500

    added pluto data

---
+++ src/data/dwarf_planets.js
@@ -0,0 +1,12 @@
+const dwarfData = {
+  "pluto": {
+    "name": "Pluto",
+    "distance": 5906440628,
+    "radius": 1151,
+    "orbit": 247.94,
+    "velocity": 16809,
+    "volume": 6387259783,
+    "gravity": 0.66,
+    "moons": 5
+  }
+}

85d14ca216e7bb528db775499608e07c94b37418
Christo Grabowski <christo.grab@gmail.com>
2023-05-09 09:36:45 -0400

    fix system views not loading properly due to variable naming error

--- src/context/PlanetContext.js
+++ src/context/PlanetContext.js
@@ -5,7 +5,7 @@ export const SystemContext = createContext();
 export default function SystemProvider(props) {
 
   // create a state variable to hold the system view, initialized to the orbit view
-  const [ systemView, setSystemView] = useState('orbit');
+  const [systemView, setSystemView] = useState('orbit');
 
   return (
     <SystemContext.Provider

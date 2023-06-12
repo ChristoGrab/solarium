d9938ebad179650409dca17680fdb6c88a49489b
Christo Grabowski <christo.grab@gmail.com>
2023-06-12 15:01:58 -0400

    UI: fix system menu to determine active view from context

--- src/components/SolarSystem/SystemMenu.js
+++ src/components/SolarSystem/SystemMenu.js
@@ -1,87 +1,42 @@
 import './SystemMenu.css';
 import { SystemContext } from "../../context/PlanetContext"
-import { useContext, useEffect } from 'react'
+import { useContext, useState } from 'react'
 
 const SystemMenu = () => {
 
-  const { setSystemView } = useContext(SystemContext)
+  const { setSystemView, systemView } = useContext(SystemContext)
+  const [activeView, setActiveView] = useState(systemView)
 
 
   // These buttons will change the state of the system view using the Context API
   // This will also change the active state of the buttons to show which view is active
   const viewOrbit = (e) => {
     e.preventDefault();
-    
-    const orbitButton = document.getElementById("orbit-button")
-    if (!orbitButton.classList.contains("is-active")) {
-      orbitButton.classList.add("is-active")
-    }
-    
-    const sizeButton = document.getElementById("size-button")
-    if (sizeButton.classList.contains("is-active")) {
-      sizeButton.classList.remove("is-active")
-    }
-    
-    const distanceButton = document.getElementById("distance-button")
-    if (distanceButton.classList.contains("is-active")) {
-      distanceButton.classList.remove("is-active")
-    }
-
     setSystemView("orbit")
-    
+    setActiveView("orbit")
   }
 
   const viewSize = (e) => {
     e.preventDefault();
-    
-    const orbitButton = document.getElementById("orbit-button")
-    if (orbitButton.classList.contains("is-active")) {
-      orbitButton.classList.remove("is-active")
-    }
-    
-    const sizeButton = document.getElementById("size-button")
-    if (!sizeButton.classList.contains("is-active")) {
-      sizeButton.classList.add("is-active")
-    }
-    
-    const distanceButton = document.getElementById("distance-button")
-    if (distanceButton.classList.contains("is-active")) {
-      distanceButton.classList.remove("is-active")
-    }
-
     setSystemView("size")
+    setActiveView("size")
   }
 
   const viewDistance = (e) => {
     e.preventDefault();
-    
-    const orbitButton = document.getElementById("orbit-button")
-    if (orbitButton.classList.contains("is-active")) {
-      orbitButton.classList.remove("is-active")
-    }
-    
-    const sizeButton = document.getElementById("size-button")
-    if (sizeButton.classList.contains("is-active")) {
-      sizeButton.classList.remove("is-active")
-    }
-    
-    const distanceButton = document.getElementById("distance-button")
-    if (!distanceButton.classList.contains("is-active")) {
-      distanceButton.classList.add("is-active")
-    }
-
     setSystemView('distance')
+    setActiveView("distance")
   }
 
   return (
     <nav className="system-type-menu">
-      <button className="system-type-button is-active"
+      <button className={`system-type-button ${activeView === 'orbit' ? 'is-active' : ''}`}
         id="orbit-button"
         onClick={viewOrbit}>Orbit</button>
-      <button className="system-type-button"
+      <button className={`system-type-button ${activeView === 'size' ? 'is-active' : ''}`}
         id="size-button"
         onClick={viewSize}>Size</button>
-      <button className="system-type-button"
+      <button className={`system-type-button ${activeView === 'distance' ? 'is-active' : ''}`}
         id="distance-button"
         onClick={viewDistance}>Distance</button>
     </nav>
